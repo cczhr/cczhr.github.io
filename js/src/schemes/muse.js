f854de61218236f6b022106d3315918f6cdb4431
cczhr <czhrockin@gmail.com>
2020-05-14 21:32:16 +0800

    Site updated: 2020-05-14 21:32:10

--- js/src/schemes/muse.js
+++ js/src/schemes/muse.js
@@ -1,9 +1,1 @@
-$(document).ready(function() {
-  function updateFooterPosition() {
-    var containerHeight = $('#footer').attr('position') ? $('.container').height() + $('#footer').outerHeight(true) : $('.container').height();
-    if (containerHeight < window.innerHeight) $('#footer').css({ 'position': 'fixed', 'bottom': 0, 'left': 0, 'right': 0 }).attr('position', 'fixed');
-    else $('#footer').removeAttr('style position');
-  }
-  updateFooterPosition();
-  $(window).on('resize scroll', updateFooterPosition);
-});
+"use strict";$(document).ready(function(){function t(){($("#footer").attr("position")?$(".container").height()+$("#footer").outerHeight(!0):$(".container").height())<window.innerHeight?$("#footer").css({position:"fixed",bottom:0,left:0,right:0}).attr("position","fixed"):$("#footer").removeAttr("style position")}t(),$(window).on("resize scroll",t)});
